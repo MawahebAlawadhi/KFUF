fa3815ce56f15274bc92e18a503898569937549f
MawahebAlawadhi <149224897+MawahebAlawadhi@users.noreply.github.com>
2024-02-28 18:09:11 +0300

    updating

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ include "home.html";
 include "about.html";
 include "menu.html";
 include "Testimonials.html";
+include "gallery.html";
 include "contact.html";
 include "footer.html"
 ?>
-
-<div>
-
-</div>
